980fc3cb2d698a9157e6d3cd66338b02807d4a56
Shilcof <robert.shilcof@gmail.com>
2020-10-21 11:08:55 +0100

    commit tree c9ef889b9564b58680c2c0ddb01b19b0dc717a70

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-
+    i = 0
+    return_values = []
+    while i < collection.size
+        return_values << yield(collection[i])
+        i += 1
+    end
+    !return_values.any?(false)
 end
